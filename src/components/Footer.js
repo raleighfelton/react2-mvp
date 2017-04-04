17291822d49567c48788de8f01faa2e1c04af4df
Michael Weslander <michael.weslander@smashingboxes.com>
2017-04-04 14:11:30 -0400

    Adds some starter css and js linting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,16 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="l-footer">
+      <div className="c-stats">
+        <span className="c-stat" href="#">
+          <img className="c-stat__icon c-stat__icon--users" src="./assets/img/users.svg" />
+          <span className="c-stat__value">456k</span>
+        </span>
+        <span className="c-stat" href="#">
+          <img className="c-stat__icon c-stat__icon--friend" src="./assets/img/friend.svg" />
+          <span className="c-stat__value">34</span>
+        </span>
+      </div>
     </footer>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,9 @@ function Header() {
   return (
     <header className="l-header">
       <a className="c-link" href="#">Back</a>
+      <a className="c-profile" href="#">
+        <img className="c-profile__icon" src="../assets/img/profile.png" alt="Profile Icon" />
+      </a>
     </header>
   );
 }
