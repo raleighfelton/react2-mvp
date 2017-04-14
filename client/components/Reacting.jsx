520bed1ce8d1326f79bcb08f8ac609845e44bd1d
Michael Weslander <michael.weslander@smashingboxes.com>
2017-04-14 17:03:29 -0400

    Removes comments

--- client/components/Reacting.jsx
+++ client/components/Reacting.jsx
@@ -1,22 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import client from 'socket.io-client';
 
 import Header from './Header';
 import Footer from './Footer';
 import Scale from './Scale';
-
-// const socket = client('http://localhost:3000');
-// // console.log(socket);
-// // const connection = socket.io;
-// const rx = {
-//   userID: 'fsadfhuhsif',
-//   reaction: 10
-// };
-// socket.emit('reaction', rx);
-// socket.on('reaction', function(msg){
-//   console.log(msg);
-// });
 
 function Reacting() {
   return (
