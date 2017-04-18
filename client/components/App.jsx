09a176749f416509370dae4e6233a24681d81f06
Michael Weslander <michael.weslander@smashingboxes.com>
2017-04-18 10:27:24 -0400

    minor updates, removes mongodb

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,20 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
+import faker from 'faker';
+import client from 'socket.io-client';
+
+const socket = client('http://localhost:3000');
 
 import Comparing from './Comparing';
 import Landing from './Landing';
 import Reacting from './Reacting';
 
-function App() {
-  return (
-    <Router>
-      <div className="body">
-        <Route exact path="/" component={Landing} />
-        <Route path="/reacting" component={Reacting} />
-        <Route path="/compare" component={Comparing} />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      avatar: faker.internet.avatar(),
+      userID: faker.internet.userName(),
+      ratings: [],
+      users: []
+    };
+
+    this.addReaction = this.addReaction.bind(this);
+    this.update = this.update.bind(this);
+
+    socket.on('reaction', this.update);
+  }
+
+  addReaction(reaction) {
+    return () => {
+      socket.emit('reaction', { userID: this.state.userID, reaction });
+    };
+  }
+
+  update(rating) {
+    this.setState({
+      ratings: this.state.ratings.concat(rating)
+    });
+  }
+
+  render() {
+    const props = {
+      ...this.state,
+      addReaction: this.addReaction
+    };
+
+    return (
+      <Router>
+        <div className="body">
+          <Route exact path="/" render={() => <Landing { ...props } />} />
+          <Route path="/reacting" render={() => <Reacting { ...props } />} />
+          <Route path="/compare" render={() => <Comparing { ...props } />} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- client/components/Scale.jsx
+++ client/components/Scale.jsx
@@ -1,83 +1,59 @@
-import React, { Component } from 'react';
-import faker from 'faker';
-import client from 'socket.io-client';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const socket = client('http://localhost:3000');
+const propTypes = {
+  addReaction: PropTypes.func.isRequired,
+  ratings: PropTypes.array.isRequired
+};
 
-class Scale extends Component {
-  constructor(props) {
-    super(props);
+function Scale({ addReaction, ratings }) {
+  console.log(ratings); // eslint-disable-line no-console
 
-    this.state = {
-      userID: faker.internet.userName(),
-      ratings: []
-    };
-
-    this.addReaction = this.addReaction.bind(this);
-    this.update = this.update.bind(this);
-
-    socket.on('reaction', this.update);
-  }
-
-  addReaction(reaction) {
-    return () => {
-      socket.emit('reaction', { userID: this.state.userID, reaction });
-    };
-  }
-
-  update(rating) {
-    this.setState({
-      ratings: this.state.ratings.concat(rating)
-    });
-  }
-
-  render() {
-    console.log(this.state.ratings); // eslint-disable-line no-console
-
-    return (
-      <div className="c-scale">
-        <div className="c-scale__tick-container" onClick={this.addReaction(100)}>
-          <div className="c-scale__tick c-scale__tick--labeled">
-            <div className="c-scale__label">+</div>
-          </div>
-        </div>
-        <div className="c-scale__tick-container" onClick={this.addReaction(90)}>
-          <div className="c-scale__tick" />
-        </div>
-        <div className="c-scale__tick-container" onClick={this.addReaction(80)}>
-          <div className="c-scale__tick" />
-        </div>
-        <div className="c-scale__tick-container" onClick={this.addReaction(70)}>
-          <div className="c-scale__tick" />
-        </div>
-        <div className="c-scale__tick-container" onClick={this.addReaction(60)}>
-          <div className="c-scale__tick" />
-        </div>
-        <div className="c-scale__tick-container" onClick={this.addReaction(50)}>
-          <div className="c-scale__tick c-scale__tick--labeled">
-            <div className="c-scale__label">N</div>
-          </div>
-        </div>
-        <div className="c-scale__tick-container" onClick={this.addReaction(40)}>
-          <div className="c-scale__tick" />
-        </div>
-        <div className="c-scale__tick-container" onClick={this.addReaction(30)}>
-          <div className="c-scale__tick" />
-        </div>
-        <div className="c-scale__tick-container" onClick={this.addReaction(20)}>
-          <div className="c-scale__tick" />
-        </div>
-        <div className="c-scale__tick-container" onClick={this.addReaction(10)}>
-          <div className="c-scale__tick" />
-        </div>
-        <div className="c-scale__tick-container" onClick={this.addReaction(0)}>
-          <div className="c-scale__tick c-scale__tick--labeled">
-            <div className="c-scale__label">-</div>
-          </div>
+  return (
+    <div className="c-scale">
+      <div className="c-scale__tick-container" onClick={addReaction(100)}>
+        <div className="c-scale__tick c-scale__tick--labeled">
+          <div className="c-scale__label">+</div>
         </div>
       </div>
-    );
-  }
+      <div className="c-scale__tick-container" onClick={addReaction(90)}>
+        <div className="c-scale__tick" />
+      </div>
+      <div className="c-scale__tick-container" onClick={addReaction(80)}>
+        <div className="c-scale__tick" />
+      </div>
+      <div className="c-scale__tick-container" onClick={addReaction(70)}>
+        <div className="c-scale__tick" />
+      </div>
+      <div className="c-scale__tick-container" onClick={addReaction(60)}>
+        <div className="c-scale__tick" />
+      </div>
+      <div className="c-scale__tick-container" onClick={addReaction(50)}>
+        <div className="c-scale__tick c-scale__tick--labeled">
+          <div className="c-scale__label">N</div>
+        </div>
+      </div>
+      <div className="c-scale__tick-container" onClick={addReaction(40)}>
+        <div className="c-scale__tick" />
+      </div>
+      <div className="c-scale__tick-container" onClick={addReaction(30)}>
+        <div className="c-scale__tick" />
+      </div>
+      <div className="c-scale__tick-container" onClick={addReaction(20)}>
+        <div className="c-scale__tick" />
+      </div>
+      <div className="c-scale__tick-container" onClick={addReaction(10)}>
+        <div className="c-scale__tick" />
+      </div>
+      <div className="c-scale__tick-container" onClick={addReaction(0)}>
+        <div className="c-scale__tick c-scale__tick--labeled">
+          <div className="c-scale__label">-</div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+Scale.propTypes = propTypes;
 
 export default Scale;
