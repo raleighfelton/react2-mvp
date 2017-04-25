3c314c9c1a84f47677636737ffffcb9b48658871
Raleigh Felton <raleighfelton@Raleighs-MacBook-Pro.local>
2017-04-25 11:10:49 -0400

    Tightened up CSS
    Feedback from Mike

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -23,10 +23,10 @@ function Landing() {
           <img src={eventImage} height="320" width="320" alt="event image" />
         </div>
         <div className="l-content">
-          <a className="c-button c-button--round" href="#">Start Reacting Anonymously</a>
-          <p>This is a proof of concept for a new platform sentiment platform React2. React2 allows you to track your emotional reactions to live events & compare it to your friends & the rest of the world, in real time.</p>
+          <a className="c-button c-button--round c-splash__button" href="#">Start Reacting Anonymously</a>
+          <p className="c-splash__p">This is a proof of concept for a new platform sentiment platform React2. React2 allows you to track your emotional reactions to live events & compare it to your friends & the rest of the world, in real time.</p>
         </div>
-        <a href="#"><span>Learn More About React2</span></a>
+        <div className="c-splash__footer" href="#"><a><span>Learn More About React2</span></a></div>
       </div>
       <hr />
       <Link to="/reacting">Welcome to React2</Link>
