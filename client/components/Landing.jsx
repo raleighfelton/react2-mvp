3d5f665ba1a7f56ea057b55d23287a44859d6de2
Raleigh Felton <raleighfelton@Raleighs-MBP.fios-router.home>
2017-05-01 23:00:53 -0400

    Updated Layout
    Re-Factored layout to flow easier and less code.

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -10,24 +10,34 @@ function Landing() {
       <div className="c-logo">
         <img src={logo} alt="React2 logo" />
       </div>
-      <div className="l-main">
-        <h1>
-          React2
-          <br />
-          Your
-          <br />
-          World
-        </h1>
-        <a href="#">Current Live Event:<span> #theGrammys</span></a>
-        <div className="c-event-image">
-          <img src={eventImage} height="320" width="320" alt="event image" />
+
+      <div className="l-main l-main--landing">
+
+        <div className="c-content">
+          <h1 className="c-headline">
+            React2
+            <br />
+            Your
+            <br />
+            World
+          </h1>
+
+          <p className="c-content__p">This is a proof of concept for a new platform sentiment platform React2. React2 allows you to track your emotional reactions to live events & compare it to your friends & the rest of the world, in real time.</p>
+          <a className="c-content__button c-button c-button--round" href="#">Start Reacting Anonymously</a>
         </div>
-        <div className="l-content">
-          <a className="c-button c-button--round c-splash__button" href="#">Start Reacting Anonymously</a>
-          <p className="c-splash__p">This is a proof of concept for a new platform sentiment platform React2. React2 allows you to track your emotional reactions to live events & compare it to your friends & the rest of the world, in real time.</p>
+
+        <div className="c-event-content">
+          <a className="c-event-content__name" href="#">Current Live Event:<span> #theGrammys</span></a>
+          <img className="c-event-image" src={eventImage} alt="event image" />
         </div>
-        <div className="c-splash__footer" href="#"><a><span>Learn More About React2</span></a></div>
+
       </div>
+
+      <footer className="c-footer">
+        <a className="c-footer__link" href="#">Learn More About React2</a>
+        <p className="c-footer__cc">©Copyright 2017 React2</p>
+      </footer>
+
       <hr />
       <Link to="/reacting">Welcome to React2</Link>
     </div>
