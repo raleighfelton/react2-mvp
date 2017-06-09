763b05628a00b260ab02b914dda7f38caf4d2f59
Brandon Mathis <bemathis@gmail.com>
2017-06-09 13:09:26 -0400

    First controller

---
+++ api/controllers/socketController.js
@@ -0,0 +1,55 @@
+const User = require('../models/user');
+const faker = require('faker');
+
+function socketController(socket, io) {
+  function broadcastConnectedUsers() {
+    User.find({ connected: true })
+      .then((users) => {
+        io.emit('connected users', users); // Broadcast all newly-connected users
+      });
+  }
+
+  // Creates a new user
+  // emits:
+  //  - new user (user object)
+  // broadcasts:
+  //  - connected users (array of all connected users including new user)
+  socket.on('new user', function() {
+    const user = new User();
+    user.avatar = faker.internet.avatar();
+    user.connected = true;
+    user.save()
+      .then((saved) => {
+        socket.emit('new user', saved); // Return new user
+        User.count({})
+          .then((count) => {
+            io.emit('users', count); // Broadcast user count
+          });
+        broadcastConnectedUsers(); // Broadcast all newly-connected users
+      })
+      .catch((err) => {
+        console.warn(`An Error Occured: ${err}`); // eslint-disable-line no-console
+      });
+  });
+
+  // Update reaction of user
+  // params: { id: '<user_id>', value: <integer 1-100> }
+  // broadcasts:
+  //  - connected users (array of all connected users)
+  socket.on('reaction', function(reaction) {
+    User.find({ _id: reaction.id })
+      .then(([user]) => {
+        user.reaction = reaction.value;
+        user.connected = true;
+        user.save()
+          .then(() => {
+            broadcastConnectedUsers(); // Broadcast all newly-connected users
+          });
+      })
+      .catch((err) => {
+        console.warn(err); // eslint-disable-line no-console
+      });
+  });
+}
+
+module.exports = { socketController };
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = require('./helpers/express');
 const http = require('http').Server(app); // eslint-disable-line new-cap
 const io = require('socket.io')(http);
 const User = require('./models/user');
+const { socketController } = require('./controllers/socketController');
 require('./config/database');
 
 app.get('/api', (req, res) => {
@@ -16,50 +17,7 @@ function broadcastConnectedUsers() {
     });
 }
 
-io.on('connection', (socket) => {
-  // Creates a new user
-  // emits:
-  //  - new user (user object)
-  // broadcasts:
-  //  - connected users (array of all connected users including new user)
-  socket.on('new user', function() {
-    const user = new User();
-    user.avatar = faker.internet.avatar();
-    user.connected = true;
-    user.save()
-      .then((saved) => {
-        socket.emit('new user', saved); // Return new user
-        User.count({})
-          .then((count) => {
-            io.emit('users', count); // Broadcast user count
-          });
-        broadcastConnectedUsers(); // Broadcast all newly-connected users
-      })
-      .catch((err) => {
-        console.warn(`An Error Occured: ${err}`); // eslint-disable-line no-console
-      });
-  });
-
-  // Update reaction of user
-  // params: { id: '<user_id>', value: <integer 1-100> }
-  // broadcasts:
-  //  - connected users (array of all connected users)
-  socket.on('reaction', function(reaction) {
-    console.log(reaction); // eslint-disable-line no-console
-    User.find({ _id: reaction.id })
-      .then(([user]) => {
-        user.reaction = reaction.value;
-        user.connected = true;
-        user.save()
-          .then(() => {
-            broadcastConnectedUsers(); // Broadcast all newly-connected users
-          });
-      })
-      .catch((err) => {
-        console.warn(err); // eslint-disable-line no-console
-      });
-  });
-});
+io.on('connection', (socket) => { socketController(socket, io); });
 
 const port = (process.env.NODE_ENV === 'production') ? process.env.PORT : 3000;
 
