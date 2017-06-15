66c0e6cc344aee87d64cbb8f51d5edebd2ce158b
Brandon Mathis <bemathis@gmail.com>
2017-06-14 23:00:09 -0400

    mLab db connection for heroku

--- api/config/database.js
+++ api/config/database.js
@@ -4,7 +4,18 @@ mongoose.Promise = global.Promise;
 if (process.env.NODE_ENV === 'test') {
   mongoose.connect('mongodb://localhost/react2_test');
 } else {
-  mongoose.connect('mongodb://localhost/react2');
+  const uristring =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/react2';
+
+  mongoose.connect(uristring, function (err, res) {
+    if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+    console.log ('Succeeded connected to: ' + uristring);
+    }
+  });
 }
 
 module.exports = mongoose;
